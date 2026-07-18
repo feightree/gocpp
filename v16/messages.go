a93adfe5a85cfedd8836e44723431d1ae27f3662
Frank Jensen <muttli@gmx.com>
2026-07-18 19:18:45 +0200

    feat: adds more message types

--- v16/messages.go
+++ v16/messages.go
@@ -17,14 +17,14 @@ type AuthorizeReq struct {
 }
 
 func (s *AuthorizeReq) UnmarshalJSON(data []byte) error {
-	type raw AuthorizeReq
-	var r raw
+	type Alias AuthorizeReq
+	var a Alias
 
-	if err := json.Unmarshal(data, &r); err != nil {
+	if err := json.Unmarshal(data, &a); err != nil {
 		return err
 	}
 
-	*s = AuthorizeReq(r)
+	*s = AuthorizeReq(a)
 	return s.Validate()
 }
 
@@ -50,14 +50,14 @@ type AuthorizeConf struct {
 }
 
 func (s *AuthorizeConf) UnmarshalJSON(data []byte) error {
-	type raw AuthorizeConf
-	var r raw
+	type Alias AuthorizeConf
+	var a Alias
 
-	if err := json.Unmarshal(data, &r); err != nil {
+	if err := json.Unmarshal(data, &a); err != nil {
 		return err
 	}
 
-	*s = AuthorizeConf(r)
+	*s = AuthorizeConf(a)
 	return s.Validate()
 }
 
@@ -102,14 +102,14 @@ type BootNotificationReq struct {
 }
 
 func (s *BootNotificationReq) UnmarshalJSON(data []byte) error {
-	type raw BootNotificationReq
-	var r raw
+	type Alias BootNotificationReq
+	var a Alias
 
-	if err := json.Unmarshal(data, &r); err != nil {
+	if err := json.Unmarshal(data, &a); err != nil {
 		return err
 	}
 
-	*s = BootNotificationReq(r)
+	*s = BootNotificationReq(a)
 	return s.Validate()
 }
 
@@ -192,14 +192,14 @@ type BootNotificationConf struct {
 }
 
 func (s *BootNotificationConf) UnmarshalJSON(data []byte) error {
-	type raw BootNotificationConf
-	var r raw
+	type Alias BootNotificationConf
+	var a Alias
 
-	if err := json.Unmarshal(data, &r); err != nil {
+	if err := json.Unmarshal(data, &a); err != nil {
 		return err
 	}
 
-	*s = BootNotificationConf(r)
+	*s = BootNotificationConf(a)
 	return s.Validate()
 }
 
@@ -212,6 +212,318 @@ func (s BootNotificationConf) Validate() error {
 		return ocpp.NewError(ocpp.ErrPropertyConstraintViolation, "interval", "must be > 0")
 	}
 
+	if s.Status == "" {
+		return ocpp.NewError(ocpp.ErrOccurenceConstraintViolation, "status", "required field is missing")
+	}
+
+	return nil
+}
+
+// CancelReservationReq (6.5)
+//
+// This contains the field definition of the CancelReservation.req PDU sent by the Central System to the Charge
+// Point. See also Cancel Reservation
+type CancelReservationReq struct {
+	// Required. Id of the reservation to cancel.
+	ReservationID int32 `json:"reservationId"`
+}
+
+func (s *CancelReservationReq) UnmarshalJSON(data []byte) error {
+	type Alias CancelReservationReq
+	var a Alias
+
+	if err := json.Unmarshal(data, &a); err != nil {
+		return err
+	}
+
+	*s = CancelReservationReq(a)
+	return s.Validate()
+}
+
+func (s CancelReservationReq) Validate() error {
+	if s.ReservationID < 1 {
+		return ocpp.NewError(ocpp.ErrPropertyConstraintViolation, "reservationId", "must be > 0")
+	}
+
+	return nil
+}
+
+// CancelReservationConf (6.6)
+//
+// This contains the field definition of the CancelReservation.conf PDU sent by the Charge Point to the Central
+// System in response to a CancelReservation.req PDU. See also Cancel Reservation
+type CancelReservationConf struct {
+	// Required. This indicates the success or failure of the cancelling of
+	// a reservation by Central System.
+	Status CancelReservationStatus `json:"status"`
+}
+
+func (s *CancelReservationConf) UnmarshalJSON(data []byte) error {
+	type Alias CancelReservationConf
+	var a Alias
+
+	if err := json.Unmarshal(data, &a); err != nil {
+		return err
+	}
+
+	*s = CancelReservationConf(a)
+	return s.Validate()
+}
+
+func (s CancelReservationConf) Validate() error {
+	if s.Status == "" {
+		return ocpp.NewError(ocpp.ErrOccurenceConstraintViolation, "status", "required field is missing")
+	}
+
+	return nil
+}
+
+// ChangeAvailabilityReq (6.7)
+//
+// This contains the field definition of the ChangeAvailability.req PDU sent by the Central System to the Charge
+// Point. See also Change Availability
+type ChangeAvailabilityReq struct {
+	// Required. The id of the connector for which availability needs to change. Id (zero) is used if the availability of the Charge Point and all its connectors needs
+	// to change.
+	ConnectorID int32 `json:"connectorId"`
+	// Required. This contains the type of availability change that the Charge Point
+	// should perform.
+	Type AvailabilityType `json:"type"`
+}
+
+func (s *ChangeAvailabilityReq) UnmarshalJSON(data []byte) error {
+	type Alias ChangeAvailabilityReq
+	var a Alias
+
+	if err := json.Unmarshal(data, &a); err != nil {
+		return err
+	}
+
+	*s = ChangeAvailabilityReq(a)
+	return s.Validate()
+}
+
+func (s ChangeAvailabilityReq) Validate() error {
+	if s.ConnectorID < 0 {
+		return ocpp.NewError(ocpp.ErrPropertyConstraintViolation, "connectorId", "must be >= 0")
+	}
+
+	if s.Type == "" {
+		return ocpp.NewError(ocpp.ErrOccurenceConstraintViolation, "type", "required field is missing")
+	}
+
+	return nil
+}
+
+// ChangeAvailabilityConf (6.8)
+//
+// This contains the field definition of the ChangeAvailability.conf PDU return by Charge Point to Central System.
+// See also Change Availability
+type ChangeAvailabilityConf struct {
+	// Required. This indicates whether the Charge Point is able to perform the
+	// availability change.
+	Status AvailabilityStatus `json:"status"`
+}
+
+func (s *ChangeAvailabilityConf) UnmarshalJSON(data []byte) error {
+	type Alias ChangeAvailabilityConf
+	var a Alias
+
+	if err := json.Unmarshal(data, &a); err != nil {
+		return err
+	}
+
+	*s = ChangeAvailabilityConf(a)
+	return s.Validate()
+}
+
+func (s ChangeAvailabilityConf) Validate() error {
+	if s.Status == "" {
+		return ocpp.NewError(ocpp.ErrOccurenceConstraintViolation, "status", "required field is missing")
+	}
+
+	return nil
+}
+
+// ChangeConfigurationReq (6.9)
+//
+// This contains the field definition of the ChangeConfiguration.req PDU sent by Central System to Charge Point. It
+// is RECOMMENDED that the content and meaning of the 'key' and 'value' fields is agreed upon between Charge
+// Point and Central System. See also Change Configuration
+type ChangeConfigurationReq struct {
+	// Required. The name of the configuration setting to change.
+	// See for standard configuration key names and associated values
+	Key CiString50Type `json:"key"`
+	// Required. The new value as string for the setting.
+	// See for standard configuration key names and associated values.
+	Value CiString500Type `json:"value"`
+}
+
+func (s *ChangeConfigurationReq) UnmarshalJSON(data []byte) error {
+	type Alias ChangeConfigurationReq
+	var a Alias
+
+	if err := json.Unmarshal(data, &a); err != nil {
+		return err
+	}
+
+	*s = ChangeConfigurationReq(a)
+	return s.Validate()
+}
+
+func (s ChangeConfigurationReq) Validate() error {
+	if s.Key == "" {
+		return ocpp.NewError(ocpp.ErrOccurenceConstraintViolation, "key", "required field is missing")
+	}
+
+	if err := s.Key.Validate(); err != nil {
+		return ocpp.WrapField("key", err)
+	}
+
+	if err := s.Value.Validate(); err != nil {
+		return ocpp.WrapField("value", err)
+	}
+
+	return nil
+}
+
+// ChangeConfigurationConf (6.10)
+//
+// This contains the field definition of the ChangeConfiguration.conf PDU returned from Charge Point to Central
+// System. See also Change Configuration
+type ChangeConfigurationConf struct {
+	// Required. Returns whether configuration change has been accepted.
+	Status ConfigurationStatus `json:"status"`
+}
+
+func (s *ChangeConfigurationConf) UnmarshalJSON(data []byte) error {
+	type Alias ChangeConfigurationConf
+	var a Alias
+
+	if err := json.Unmarshal(data, &a); err != nil {
+		return err
+	}
+
+	*s = ChangeConfigurationConf(a)
+	return s.Validate()
+}
+
+func (s ChangeConfigurationConf) Validate() error {
+	if s.Status == "" {
+		return ocpp.NewError(ocpp.ErrOccurenceConstraintViolation, "status", "required field is missing")
+	}
+
+	return nil
+}
+
+// ClearCacheReq (6.11)
+//
+// This contains the field definition of the ClearCache.req PDU sent by the Central System to the Charge Point. See
+// also Clear Cache
+//
+// No fields are defined.
+type ClearCacheReq struct{}
+
+// ClearCacheConf (6.12)
+//
+// This contains the field definition of the ClearCache.conf PDU sent by the Charge Point to the Central System in
+// response to a ClearCache.req PDU. See also Clear Cache
+type ClearCacheConf struct {
+	// Required. Accepted if the Charge Point has executed the request, otherwise rejected.
+	Status ClearCacheStatus `json:"status"`
+}
+
+func (s *ClearCacheConf) UnmarshalJSON(data []byte) error {
+	type Alias ClearCacheConf
+	var a Alias
+
+	if err := json.Unmarshal(data, &a); err != nil {
+		return err
+	}
+
+	*s = ClearCacheConf(a)
+	return s.Validate()
+}
+
+func (s ClearCacheConf) Validate() error {
+	if s.Status == "" {
+		return ocpp.NewError(ocpp.ErrOccurenceConstraintViolation, "status", "required field is missing")
+	}
+
+	return nil
+}
+
+// ClearChargingProfileReq (6.13)
+//
+// This contains the field definition of the ClearChargingProfile.req PDU sent by the Central System to the Charge
+// Point.
+//
+// The Central System can use this message to clear (remove) either a specific charging profile (denoted by id) or a
+// selection of charging profiles that match with the values of the optional connectorId, stackLevel and
+// chargingProfilePurpose fields. See also Clear Charging Profile
+type ClearChargingProfileReq struct {
+	// Optional. The ID of the charging profile to clear.
+	ID *int32 `json:"id,omitempty"`
+	// Optional. Specifies the ID of the connector for which to clear
+	// charging profiles. A connectorId of zero (0) specifies the charging
+	// profile for the overall Charge Point. Absence of this parameter
+	// means the clearing applies to all charging profiles that match the
+	// other criteria in the request.
+	ConnectorID *int32 `json:"connectorId,omitempty"`
+	// Optional. Specifies to purpose of the charging profiles that will be
+	// cleared, if they meet the other criteria in the request.
+	ChargingProfilePurpose *ChargingProfilePurposeType `json:"chargingProfilePurpose,omitempty"`
+	// Optional. specifies the stackLevel for which charging profiles will
+	// be cleared, if they meet the other criteria in the request
+	StackLevel *int32 `json:"stackLevel,omitempty"`
+}
+
+func (s *ClearChargingProfileReq) UnmarshalJSON(data []byte) error {
+	type Alias ClearChargingProfileReq
+	var a Alias
+
+	if err := json.Unmarshal(data, &a); err != nil {
+		return err
+	}
+
+	*s = ClearChargingProfileReq(a)
+	return s.Validate()
+}
+
+func (s ClearChargingProfileReq) Validate() error {
+	if s.ConnectorID != nil && *s.ConnectorID < 0 {
+		return ocpp.NewError(ocpp.ErrPropertyConstraintViolation, "connectorId", "must be >= 0")
+	}
+
+	if s.StackLevel != nil && *s.StackLevel < 0 {
+		return ocpp.NewError(ocpp.ErrPropertyConstraintViolation, "stackLevel", "must be >= 0")
+	}
+
+	return nil
+}
+
+// ClearChargingProfileConf (6.14)
+//
+// This contains the field definition of the ClearChargingProfile.conf PDU sent by the Charge Point to the Central
+// System in response to a ClearChargingProfile.req PDU. See also Clear Charging Profile
+type ClearChargingProfileConf struct {
+	// Required. Indicates if the Charge Point was able to execute the request.
+	Status ClearChargingProfileStatus `json:"status"`
+}
+
+func (s *ClearChargingProfileConf) UnmarshalJSON(data []byte) error {
+	type Alias ClearChargingProfileConf
+	var a Alias
+
+	if err := json.Unmarshal(data, &a); err != nil {
+		return err
+	}
+
+	*s = ClearChargingProfileConf(a)
+	return s.Validate()
+}
+
+func (s ClearChargingProfileConf) Validate() error {
 	if s.Status == "" {
 		return ocpp.NewError(ocpp.ErrOccurenceConstraintViolation, "status", "required field is missing")
 	}
--- v16/messages_test.go
+++ v16/messages_test.go
@@ -297,3 +297,567 @@ func TestBootNotificationConfValidate(t *testing.T) {
 		})
 	}
 }
+
+func TestCancelReservationReqUnmarshal(t *testing.T) {
+	t.Run("rejects zero reservationId", func(t *testing.T) {
+		var v CancelReservationReq
+		err := json.Unmarshal([]byte(`{"reservationId":0}`), &v)
+
+		if !errors.Is(err, ocpp.ErrPropertyConstraintViolation) {
+			t.Errorf("got %v, want %v", err, ocpp.ErrPropertyConstraintViolation)
+		}
+	})
+
+	t.Run("valid input", func(t *testing.T) {
+		var v CancelReservationReq
+		err := json.Unmarshal([]byte(`{"reservationId":42}`), &v)
+
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+
+		if v.ReservationID != 42 {
+			t.Errorf("got %d, want %d", v.ReservationID, 42)
+		}
+	})
+}
+
+func TestCancelReservationReqValidate(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   CancelReservationReq
+		wantErr error
+	}{
+		{
+			name:    "accepts valid input",
+			input:   CancelReservationReq{ReservationID: 1},
+			wantErr: nil,
+		},
+		{
+			name:    "rejects zero reservationId",
+			input:   CancelReservationReq{ReservationID: 0},
+			wantErr: ocpp.ErrPropertyConstraintViolation,
+		},
+		{
+			name:    "rejects negative reservationId",
+			input:   CancelReservationReq{ReservationID: -1},
+			wantErr: ocpp.ErrPropertyConstraintViolation,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.input.Validate()
+
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("unexpected error\ngot:  %v\nwant: %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestCancelReservationConfUnmarshal(t *testing.T) {
+	t.Run("rejects invalid status", func(t *testing.T) {
+		var v CancelReservationConf
+		err := json.Unmarshal([]byte(`{"status":"BadStatus"}`), &v)
+
+		if !errors.Is(err, ocpp.ErrPropertyConstraintViolation) {
+			t.Errorf("got %v, want %v", err, ocpp.ErrPropertyConstraintViolation)
+		}
+	})
+
+	t.Run("valid input", func(t *testing.T) {
+		var v CancelReservationConf
+		err := json.Unmarshal([]byte(`{"status":"Accepted"}`), &v)
+
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+
+		if v.Status != CancelReservationStatusAccepted {
+			t.Errorf("got %q, want %q", v.Status, CancelReservationStatusAccepted)
+		}
+	})
+}
+
+func TestCancelReservationConfValidate(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   CancelReservationConf
+		wantErr error
+	}{
+		{
+			name:    "accepts valid input",
+			input:   CancelReservationConf{Status: CancelReservationStatusAccepted},
+			wantErr: nil,
+		},
+		{
+			name:    "rejects empty status",
+			input:   CancelReservationConf{Status: ""},
+			wantErr: ocpp.ErrOccurenceConstraintViolation,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.input.Validate()
+
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("unexpected error\ngot:  %v\nwant: %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestChangeAvailabilityReqUnmarshal(t *testing.T) {
+	t.Run("rejects invalid type", func(t *testing.T) {
+		var v ChangeAvailabilityReq
+		err := json.Unmarshal([]byte(`{"connectorId":1,"type":"BadType"}`), &v)
+
+		if !errors.Is(err, ocpp.ErrPropertyConstraintViolation) {
+			t.Errorf("got %v, want %v", err, ocpp.ErrPropertyConstraintViolation)
+		}
+	})
+
+	t.Run("valid input", func(t *testing.T) {
+		var v ChangeAvailabilityReq
+		err := json.Unmarshal([]byte(`{"connectorId":1,"type":"Operative"}`), &v)
+
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+
+		if v.ConnectorID != 1 {
+			t.Errorf("got %d, want %d", v.ConnectorID, 1)
+		}
+
+		if v.Type != AvailabilityTypeOperative {
+			t.Errorf("got %q, want %q", v.Type, AvailabilityTypeOperative)
+		}
+	})
+}
+
+func TestChangeAvailabilityReqValidate(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   ChangeAvailabilityReq
+		wantErr error
+	}{
+		{
+			name:    "accepts valid input",
+			input:   ChangeAvailabilityReq{ConnectorID: 1, Type: AvailabilityTypeOperative},
+			wantErr: nil,
+		},
+		{
+			name:    "accepts zero connectorId",
+			input:   ChangeAvailabilityReq{ConnectorID: 0, Type: AvailabilityTypeOperative},
+			wantErr: nil,
+		},
+		{
+			name:    "rejects negative connectorId",
+			input:   ChangeAvailabilityReq{ConnectorID: -1, Type: AvailabilityTypeOperative},
+			wantErr: ocpp.ErrPropertyConstraintViolation,
+		},
+		{
+			name:    "rejects empty type",
+			input:   ChangeAvailabilityReq{ConnectorID: 1, Type: ""},
+			wantErr: ocpp.ErrOccurenceConstraintViolation,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.input.Validate()
+
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("unexpected error\ngot:  %v\nwant: %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestChangeAvailabilityConfUnmarshal(t *testing.T) {
+	t.Run("rejects invalid status", func(t *testing.T) {
+		var v ChangeAvailabilityConf
+		err := json.Unmarshal([]byte(`{"status":"BadStatus"}`), &v)
+
+		if !errors.Is(err, ocpp.ErrPropertyConstraintViolation) {
+			t.Errorf("got %v, want %v", err, ocpp.ErrPropertyConstraintViolation)
+		}
+	})
+
+	t.Run("valid input", func(t *testing.T) {
+		var v ChangeAvailabilityConf
+		err := json.Unmarshal([]byte(`{"status":"Accepted"}`), &v)
+
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+
+		if v.Status != AvailabilityStatusAccepted {
+			t.Errorf("got %q, want %q", v.Status, AvailabilityStatusAccepted)
+		}
+	})
+}
+
+func TestChangeAvailabilityConfValidate(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   ChangeAvailabilityConf
+		wantErr error
+	}{
+		{
+			name:    "accepts valid input",
+			input:   ChangeAvailabilityConf{Status: AvailabilityStatusAccepted},
+			wantErr: nil,
+		},
+		{
+			name:    "rejects empty status",
+			input:   ChangeAvailabilityConf{Status: ""},
+			wantErr: ocpp.ErrOccurenceConstraintViolation,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.input.Validate()
+
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("unexpected error\ngot:  %v\nwant: %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestChangeConfigurationReqUnmarshal(t *testing.T) {
+	t.Run("rejects missing key", func(t *testing.T) {
+		var v ChangeConfigurationReq
+		err := json.Unmarshal([]byte(`{"value":"someValue"}`), &v)
+
+		if !errors.Is(err, ocpp.ErrOccurenceConstraintViolation) {
+			t.Errorf("got %v, want %v", err, ocpp.ErrOccurenceConstraintViolation)
+		}
+	})
+
+	t.Run("rejects oversized key", func(t *testing.T) {
+		var v ChangeConfigurationReq
+		err := json.Unmarshal([]byte(`{"key":"ThisKeyIsOverFiftyCharactersLongAndShouldBeRejected","value":"someValue"}`), &v)
+
+		if !errors.Is(err, ocpp.ErrPropertyConstraintViolation) {
+			t.Errorf("got %v, want %v", err, ocpp.ErrPropertyConstraintViolation)
+		}
+	})
+
+	t.Run("accepts empty value", func(t *testing.T) {
+		var v ChangeConfigurationReq
+		err := json.Unmarshal([]byte(`{"key":"someKey","value":""}`), &v)
+
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+	})
+
+	t.Run("valid input", func(t *testing.T) {
+		var v ChangeConfigurationReq
+		err := json.Unmarshal([]byte(`{"key":"someKey","value":"someValue"}`), &v)
+
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+
+		if v.Key != "someKey" {
+			t.Errorf("got %q, want %q", v.Key, "someKey")
+		}
+
+		if v.Value != "someValue" {
+			t.Errorf("got %q, want %q", v.Value, "someValue")
+		}
+	})
+}
+
+func TestChangeConfigurationReqValidate(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   ChangeConfigurationReq
+		wantErr error
+	}{
+		{
+			name:    "accepts valid input",
+			input:   ChangeConfigurationReq{Key: "someKey", Value: "someValue"},
+			wantErr: nil,
+		},
+		{
+			name:    "accepts empty value",
+			input:   ChangeConfigurationReq{Key: "someKey", Value: ""},
+			wantErr: nil,
+		},
+		{
+			name:    "rejects empty key",
+			input:   ChangeConfigurationReq{Key: "", Value: "someValue"},
+			wantErr: ocpp.ErrOccurenceConstraintViolation,
+		},
+		{
+			name:    "rejects oversized key",
+			input:   ChangeConfigurationReq{Key: "ThisKeyIsOverFiftyCharactersLongAndShouldBeRejected", Value: "someValue"},
+			wantErr: ocpp.ErrPropertyConstraintViolation,
+		},
+		{
+			name:    "rejects oversized value",
+			input:   ChangeConfigurationReq{Key: "someKey", Value: CiString500Type("x" + string(make([]byte, 500)))},
+			wantErr: ocpp.ErrPropertyConstraintViolation,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.input.Validate()
+
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("unexpected error\ngot:  %v\nwant: %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestChangeConfigurationConfUnmarshal(t *testing.T) {
+	t.Run("rejects invalid status", func(t *testing.T) {
+		var v ChangeConfigurationConf
+		err := json.Unmarshal([]byte(`{"status":"BadStatus"}`), &v)
+
+		if !errors.Is(err, ocpp.ErrPropertyConstraintViolation) {
+			t.Errorf("got %v, want %v", err, ocpp.ErrPropertyConstraintViolation)
+		}
+	})
+
+	t.Run("valid input", func(t *testing.T) {
+		var v ChangeConfigurationConf
+		err := json.Unmarshal([]byte(`{"status":"Accepted"}`), &v)
+
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+
+		if v.Status != ConfigurationStatusAccepted {
+			t.Errorf("got %q, want %q", v.Status, ConfigurationStatusAccepted)
+		}
+	})
+}
+
+func TestChangeConfigurationConfValidate(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   ChangeConfigurationConf
+		wantErr error
+	}{
+		{
+			name:    "accepts valid input",
+			input:   ChangeConfigurationConf{Status: ConfigurationStatusAccepted},
+			wantErr: nil,
+		},
+		{
+			name:    "rejects empty status",
+			input:   ChangeConfigurationConf{Status: ""},
+			wantErr: ocpp.ErrOccurenceConstraintViolation,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.input.Validate()
+
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("unexpected error\ngot:  %v\nwant: %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestClearCacheReqUnmarshal(t *testing.T) {
+	t.Run("valid input", func(t *testing.T) {
+		var v ClearCacheReq
+		err := json.Unmarshal([]byte(`{}`), &v)
+
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+	})
+}
+
+func TestClearCacheConfUnmarshal(t *testing.T) {
+	t.Run("rejects invalid status", func(t *testing.T) {
+		var v ClearCacheConf
+		err := json.Unmarshal([]byte(`{"status":"BadStatus"}`), &v)
+
+		if !errors.Is(err, ocpp.ErrPropertyConstraintViolation) {
+			t.Errorf("got %v, want %v", err, ocpp.ErrPropertyConstraintViolation)
+		}
+	})
+
+	t.Run("valid input", func(t *testing.T) {
+		var v ClearCacheConf
+		err := json.Unmarshal([]byte(`{"status":"Accepted"}`), &v)
+
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+
+		if v.Status != ClearCacheStatusAccepted {
+			t.Errorf("got %q, want %q", v.Status, ClearCacheStatusAccepted)
+		}
+	})
+}
+
+func TestClearCacheConfValidate(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   ClearCacheConf
+		wantErr error
+	}{
+		{
+			name:    "accepts valid input",
+			input:   ClearCacheConf{Status: ClearCacheStatusAccepted},
+			wantErr: nil,
+		},
+		{
+			name:    "rejects empty status",
+			input:   ClearCacheConf{Status: ""},
+			wantErr: ocpp.ErrOccurenceConstraintViolation,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.input.Validate()
+
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("unexpected error\ngot:  %v\nwant: %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestClearChargingProfileReqUnmarshal(t *testing.T) {
+	t.Run("rejects invalid chargingProfilePurpose", func(t *testing.T) {
+		var v ClearChargingProfileReq
+		err := json.Unmarshal([]byte(`{"chargingProfilePurpose":"BadPurpose"}`), &v)
+
+		if !errors.Is(err, ocpp.ErrPropertyConstraintViolation) {
+			t.Errorf("got %v, want %v", err, ocpp.ErrPropertyConstraintViolation)
+		}
+	})
+
+	t.Run("valid input with no fields", func(t *testing.T) {
+		var v ClearChargingProfileReq
+		err := json.Unmarshal([]byte(`{}`), &v)
+
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+	})
+
+	t.Run("valid input with all fields", func(t *testing.T) {
+		var v ClearChargingProfileReq
+		err := json.Unmarshal([]byte(`{"id":1,"connectorId":0,"chargingProfilePurpose":"TxProfile","stackLevel":0}`), &v)
+
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+	})
+}
+
+func TestClearChargingProfileReqValidate(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   ClearChargingProfileReq
+		wantErr error
+	}{
+		{
+			name:    "accepts empty request",
+			input:   ClearChargingProfileReq{},
+			wantErr: nil,
+		},
+		{
+			name:    "accepts zero connectorId",
+			input:   ClearChargingProfileReq{ConnectorID: ptr(int32(0))},
+			wantErr: nil,
+		},
+		{
+			name:    "accepts zero stackLevel",
+			input:   ClearChargingProfileReq{StackLevel: ptr(int32(0))},
+			wantErr: nil,
+		},
+		{
+			name:    "rejects negative connectorId",
+			input:   ClearChargingProfileReq{ConnectorID: ptr(int32(-1))},
+			wantErr: ocpp.ErrPropertyConstraintViolation,
+		},
+		{
+			name:    "rejects negative stackLevel",
+			input:   ClearChargingProfileReq{StackLevel: ptr(int32(-1))},
+			wantErr: ocpp.ErrPropertyConstraintViolation,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.input.Validate()
+
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("unexpected error\ngot:  %v\nwant: %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestClearChargingProfileConfUnmarshal(t *testing.T) {
+	t.Run("rejects invalid status", func(t *testing.T) {
+		var v ClearChargingProfileConf
+		err := json.Unmarshal([]byte(`{"status":"BadStatus"}`), &v)
+
+		if !errors.Is(err, ocpp.ErrPropertyConstraintViolation) {
+			t.Errorf("got %v, want %v", err, ocpp.ErrPropertyConstraintViolation)
+		}
+	})
+
+	t.Run("valid input", func(t *testing.T) {
+		var v ClearChargingProfileConf
+		err := json.Unmarshal([]byte(`{"status":"Accepted"}`), &v)
+
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+
+		if v.Status != ClearChargingProfileStatusAccepted {
+			t.Errorf("got %q, want %q", v.Status, ClearChargingProfileStatusAccepted)
+		}
+	})
+}
+
+func TestClearChargingProfileConfValidate(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   ClearChargingProfileConf
+		wantErr error
+	}{
+		{
+			name:    "accepts valid input",
+			input:   ClearChargingProfileConf{Status: ClearChargingProfileStatusAccepted},
+			wantErr: nil,
+		},
+		{
+			name:    "rejects empty status",
+			input:   ClearChargingProfileConf{Status: ""},
+			wantErr: ocpp.ErrOccurenceConstraintViolation,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.input.Validate()
+
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("unexpected error\ngot:  %v\nwant: %v", err, tt.wantErr)
+			}
+		})
+	}
+}
